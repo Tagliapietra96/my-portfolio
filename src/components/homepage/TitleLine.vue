556abbc129604528d561503b8c83b0c1c98424a1
Tagliapietra96 <tagliatek96@gmail.com>
2023-03-29 20:10:18 +0200

    removed hover fx from the static title

--- src/components/homepage/TitleLine.vue
+++ src/components/homepage/TitleLine.vue
@@ -21,18 +21,15 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../../scss/partials/variables/spacing.scss';
 span {
     cursor: default;
-    line-height: calc($big + .5rem);
     filter: drop-shadow(0px 10px 25px rgba(255, 255, 255, 0.3));
     -webkit-filter: drop-shadow(0px 10px 25px rgba(255, 255, 255, 0.3));
-    transition: filter .3s, -webkit-filter .3s, font-size .3s;
+    transition: filter .3s, -webkit-filter .3s;
 
     &.active:hover {
         filter: drop-shadow(0px 10px 25px rgba(255, 255, 255, 1));
         -webkit-filter: drop-shadow(0px 10px 25px rgba(255, 255, 255, 1));
-        font-size: calc($big + .5rem);
     }
 }
 </style>
